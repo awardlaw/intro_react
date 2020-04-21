ad8ec462e90051a8ecd6309298c4b89b7267efe9
Alison Wardlaw <alison.wardlaw@raventelemetry.com>
2020-04-21 18:24:19 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+// a "Component"
+export function App() {
+
+  // [the value, the setter for that value]
+  const [number, setNumber] = useState(2)
+
+  // Components return JSX ("HTML in Javascript")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* in JSX land */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          // back in JS land
+          // write expressions in curly brackets
+          number}
+
       </header>
+      <button onClick={function () {
+        const newNumber = multiplyNumberBy2(number)
+
+        // how do we update the page?
+
+        // trigger a re-render
+        setNumber(newNumber)
+
+      }}>
+        Multiply by 2!
+      </button>
     </div>
   );
 }
 
-export default App;
+function multiplyNumberBy2(number) {
+  console.log("🌟🚨: number", number)
+  return number * 2
+}
